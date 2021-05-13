f9e4b3aa8bef023df7c2a7e3db7d7e6a117c2e30
youcef101 <youcef.khadem12@gmail.com>
2021-05-13 17:30:23 +0100

    add Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 // import Movies from './Movies'
-// import Viewers from './Viewers'
+import Viewers from './Viewers'
 
 function Home() {
     return (
         <Container>
             <ImgSlider />
-            {/* <Viewers />
-            <Movies /> */}
+            <Viewers />
+            {/* <Movies />  */}
         </Container>
     )
 }
